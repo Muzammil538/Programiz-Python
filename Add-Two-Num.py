79b4c3946c8f1a014ccdd7a5d5ae3256bdf92fa7
Muzammil538 <Muzammil538@users.noreply.github.com>
2020-11-26 16:59:51 +0530

    py

---
+++ Add-Two-Num.py
@@ -0,0 +1,11 @@
+#This is the first example
+num1 = 15
+num2 = 195
+
+#Adding two numbers
+sum = num1+num2
+print(num1 ,"+" num2, "=",sum)
+
+#The second example 
+n1 = float(input("Give a Number :"))
+n2 = float(input("Give a Number :"))
---
+++ Hello-World.py
@@ -0,0 +1,3 @@
+#This is Hello World Program
+
+print("Hello , World!") 
